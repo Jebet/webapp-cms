192d8f8bdd7079fb16d71de651046754f48ff100
eclay jebet <eclay.jebet@shujaaz.com>
2019-05-24 12:37:15 +0300

    Update newTip.js
    Add Card title which will inform users where they are currently are. Also made modification to the styling and change the input fields to facilitate typing

--- src/Tips/newTip.js
+++ src/Tips/newTip.js
@@ -17,6 +17,10 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormGroup from "@material-ui/core/FormGroup";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+
 import "./newTip.css";
 
 const styles = theme => ({
@@ -49,9 +53,24 @@ const styles = theme => ({
     }),
     marginLeft: 0
   },
+  typography: {
+    fontSize: "3rem",
+    "@media (min-width:600px)": {
+      fontSize: "4.5rem"
+      // color: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "6rem"
+    }
+  },
   card: {
     minWidth: 275,
-    height: 610
+    height: 580
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    minWidth: 350
   },
   cssRoot: {
     color: theme.palette.getContrastText(purple[500]),
@@ -61,6 +80,11 @@ const styles = theme => ({
     }
   }
 });
+
+// const [values, setValues] = React.useState({
+//   multiline: "Controlled",
+//   currency: "EUR"
+// });
 
 class Tips extends React.Component {
   state = {
@@ -79,130 +103,155 @@ class Tips extends React.Component {
 
     return (
       <div className="tip-container">
-      <div className={classes.root} style={{ padding: "15px" }}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Grid container spacing={24}>
-              <Grid item xs={12} sm={8}>
-                <Paper className={classes.paper} style={{ height: "350px" }}>
-                  <FileUploader style={{ position: "relative", top: "50px" }} />
-                  {/* <div
-              class="fas fa-cloud-upload-alt"
-              style={{ width: "60px", height: "60px", color: "red" }}
-            /> */}
-                </Paper>
+        <div className={classes.root} style={{ padding: "15px" }}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography className={"heading"} variant={"h5"} gutterBottom>
+                ADD NEW TIPS
+              </Typography>
+
+              <Grid container spacing={24}>
+                <Grid item xs={12} sm={8}>
+                  <Paper className={classes.paper} style={{ height: "400px" }}>
+                    <FileUploader />
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    id="outlined-mind-input"
+                    label="Tip Title"
+                    className={classes.textField}
+                    type="text"
+                    name="What's on your mind?"
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={8} style={{ height: "10px" }} />
+
+                <Grid item xs={12} sm={4}>
+                  <TextField
+                    id="outlined-multiline-flexible"
+                    label="Multiline"
+                    multiline
+                    rowsMax="4"
+                    // value={values.multiline}
+                    // onChange={handleChange("multiline")}
+                    className={classes.textField}
+                    margin="normal"
+                    helperText="hello"
+                    variant="outlined"
+                    style={{
+                      position: "relative",
+                      bottom: "340px",
+                      height: "230px"
+                    }}
+                  />
+                  {/* <Paper
+                    className={classes.paper}
+                    style={{
+                      position: "relative",
+                      bottom: "340px",
+                      height: "130px"
+                    }}
+                  >
+                    Description
+                  </Paper> */}
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className={classNames(classes.margin, classes.cssRoot)}
+                    style={{
+                      height: "40px",
+                      minWidth: "70px",
+                      position: "relative",
+                      backgroundcolor: "purple",
+                      float: "right",
+                      bottom: "313px"
+                    }}
+                  >
+                    CREATE
+                  </Button>
+
+                  {/* <Grid item xs={12} sm={4} > */}
+                  <div className="checkbox-wrapper" col-sm-4>
+                    <FormGroup row>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={this.state.checkedA}
+                            onChange={this.handleChange("checkedA")}
+                            value="checkedA"
+                          />
+                        }
+                        label="Secondary"
+                      />
+
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={this.state.checkedB}
+                            onChange={this.handleChange("checkedB")}
+                            value="checkedB"
+                            color="primary"
+                          />
+                        }
+                        label="Primary"
+                      />
+
+                      <FormControlLabel
+                        control={<Checkbox value="checkedC" />}
+                        label="Uncontrolled"
+                      />
+
+                      <br />
+
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            checked={this.state.checkedG}
+                            onChange={this.handleChange("checkedG")}
+                            value="checkedG"
+                            classes={{
+                              root: classes.root,
+                              checked: classes.checked
+                            }}
+                          />
+                        }
+                        label="Custom color"
+                      />
+
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            icon={<FavoriteBorder />}
+                            checkedIcon={<Favorite />}
+                            value="checkedH"
+                          />
+                        }
+                        label="Custom icon"
+                      />
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+                            checkedIcon={<CheckBoxIcon fontSize="small" />}
+                            value="checkedI"
+                          />
+                        }
+                        label="Custom size"
+                      />
+                    </FormGroup>
+                  </div>
+                  {/* </Grid> */}
+                </Grid>
               </Grid>
-
-              <Grid item xs={12} sm={4}>
-                <Paper className={classes.paper}>Tip Name</Paper>
-              </Grid>
-
-              <Grid item xs={12} sm={8} />
-
-              <Grid item xs={12} sm={4}>
-                <Paper
-                  className={classes.paper}
-                  style={{
-                    position: "relative",
-                    bottom: "300px",
-                    height: "130px"
-                  }}
-                >
-                  Description
-                </Paper>
-
-                <Button
-                  variant="contained"
-                  color="primary"
-                  className={classNames(classes.margin, classes.cssRoot)}
-                  style={{
-                    height: "40px",
-                    minWidth: "70px",
-                    position: "relative",
-                    backgroundcolor: "purple",
-                    float: "right",
-                    bottom: "20px"
-                  }}
-                >
-                  CREATE
-                </Button>
-
-                {/* <Grid item xs={12} sm={4} > */}
-                <div className="checkbox-wrapper" col-sm-4>
-                  <FormGroup row>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={this.state.checkedA}
-                          onChange={this.handleChange("checkedA")}
-                          value="checkedA"
-                        />
-                      }
-                      label="Secondary"
-                    />
-
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={this.state.checkedB}
-                          onChange={this.handleChange("checkedB")}
-                          value="checkedB"
-                          color="primary"
-                        />
-                      }
-                      label="Primary"
-                    />
-
-                    <FormControlLabel
-                      control={<Checkbox value="checkedC" />}
-                      label="Uncontrolled"
-                    />
-
-                    <br />
-
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          checked={this.state.checkedG}
-                          onChange={this.handleChange("checkedG")}
-                          value="checkedG"
-                          classes={{
-                            root: classes.root,
-                            checked: classes.checked
-                          }}
-                        />
-                      }
-                      label="Custom color"
-                    />
-
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          icon={<FavoriteBorder />}
-                          checkedIcon={<Favorite />}
-                          value="checkedH"
-                        />
-                      }
-                      label="Custom icon"
-                    />
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
-                          checkedIcon={<CheckBoxIcon fontSize="small" />}
-                          value="checkedI"
-                        />
-                      }
-                      label="Custom size"
-                    />
-                  </FormGroup>
-                </div>
-                {/* </Grid> */}
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     );
   }
