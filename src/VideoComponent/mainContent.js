cacfbd7be04b0bfd79dd374ff8c8c0efb2a5bb8f
eclay jebet <eclay.jebet@shujaaz.com>
2019-05-27 11:52:20 +0300

    Update mainContent.js
    Code cleanUp - removing useless imports

--- src/VideoComponent/mainContent.js
+++ src/VideoComponent/mainContent.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
+// import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import green from "@material-ui/core/colors/green";
