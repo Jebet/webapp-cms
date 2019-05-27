0ced6aba63561f35d5979dc42d39449b79cebcee
eclay jebet <eclay.jebet@shujaaz.com>
2019-05-27 11:52:46 +0300

    Update App.js
    Code cleanUp - removing useless imports(Links & routes)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
 import PersistentDrawerLeft from "./SideNavigation/SideNav";
-import Video from "./VideoComponent/Videobackground";
+// import Video from "./VideoComponent/Videobackground";
 import Tips from "./Tips/newTip";
 import MainContent from "./VideoComponent/mainContent";
 
